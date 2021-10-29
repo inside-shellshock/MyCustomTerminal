20f442b2d16db084cf9fd37bd905746572be0769
Alessandro <alex.95.speed@gmail.com>
2021-10-29 12:59:12 +0200

    Update getif.sh

--- getif.sh
+++ getif.sh
@@ -1,3 +1,13 @@
-check_eth=$(ipconfig.exe | grep 'YOUR_ETHERNET_INTERFACE_NAME' -A4 | cut -d":" -f 2 | tail -n1 | sed -e 's/\s*//g')
-check_dock_eth=$(ipconfig.exe | grep 'YOUR_SECONDARY_ETHERNET_INTERFACE_NAME' -A4 | cut -d":" -f 2 | tail -n1 | sed -e 's/\s*//g')
-if [[ -z $check_eth ]]; then echo $check_dock_eth; fi
+check_wifi=$(ipconfig.exe | grep 'wifiETH' -A4 | cut -d":" -f 2 | tail -n1 | sed -e 's/\s*//g')
+check_eth=$(ipconfig.exe | grep 'EthernetDell' -A4 | cut -d":" -f 2 | tail -n1 | sed -e 's/\s*//g')
+check_dock_eth=$(ipconfig.exe | grep 'DockingETH' -A4 | cut -d":" -f 2 | tail -n1 | sed -e 's/\s*//g')
+if [[ -z $check_wifi ]]; then
+        if [[ -z $check_eth ]]; then
+                 echo $check_dock_eth;
+        else
+                echo $check_eth;
+        fi
+else
+        echo $check_wifi;
+
+fi
